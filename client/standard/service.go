b2a3df6180ac9aee43fe5f6f62b230818832b6b9
AntiD2ta <mtpotro41@gmail.com>
2026-01-12 15:25:48 +0100

    Implement Client Certificate Manager (client/standard/)
    - Extracted from vouch's credentialsFromCerts patterns
    - Supports optional CA certificates
    - TLS 1.3 minimum enforcement
    - Lazy certificate fetching

---
+++ client/standard/service.go
@@ -0,0 +1,103 @@
+// Copyright © 2026 Attestant Limited.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package standard
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+
+	"github.com/attestantio/go-certmanager/client"
+	"github.com/attestantio/go-certmanager/fetcher"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	zerologger "github.com/rs/zerolog/log"
+)
+
+// Service is the standard client certificate manager implementation.
+type Service struct {
+	fetcher    fetcher.Fetcher
+	certPEMURI string
+	certKeyURI string
+	caCertURI  string
+}
+
+var _ client.Service = (*Service)(nil)
+
+var log zerolog.Logger
+
+// New creates a new client certificate manager.
+func New(ctx context.Context, params ...Parameter) (*Service, error) {
+	parameters, err := parseAndCheckParameters(params...)
+	if err != nil {
+		return nil, errors.Wrap(err, "problem with parameters")
+	}
+
+	log = zerologger.With().Str("service", "certmanager").Str("impl", "client").Str("type", "standard").Logger()
+	if parameters.logLevel != log.GetLevel() {
+		log = log.Level(parameters.logLevel)
+	}
+
+	return &Service{
+		fetcher:    parameters.fetcher,
+		certPEMURI: parameters.certPEMURI,
+		certKeyURI: parameters.certKeyURI,
+		caCertURI:  parameters.caCertURI,
+	}, nil
+}
+
+func (s *Service) GetCertificatePair(ctx context.Context) (*tls.Certificate, error) {
+	clientCert, err := s.fetcher.Fetch(ctx, s.certPEMURI)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to obtain client certificate")
+	}
+	clientKey, err := s.fetcher.Fetch(ctx, s.certKeyURI)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to obtain client key")
+	}
+
+	clientPair, err := tls.X509KeyPair(clientCert, clientKey)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to load client keypair")
+	}
+
+	return &clientPair, nil
+}
+
+func (s *Service) GetTLSConfig(ctx context.Context) (*tls.Config, error) {
+	clientPair, err := s.GetCertificatePair(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	tlsCfg := &tls.Config{
+		Certificates: []tls.Certificate{*clientPair},
+		MinVersion:   tls.VersionTLS13,
+	}
+
+	// Add CA certificate if specified.
+	if s.caCertURI != "" {
+		caCert, err := s.fetcher.Fetch(ctx, s.caCertURI)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to obtain CA certificate")
+		}
+		cp := x509.NewCertPool()
+		if !cp.AppendCertsFromPEM(caCert) {
+			return nil, errors.New("failed to add CA certificate")
+		}
+		tlsCfg.RootCAs = cp
+	}
+
+	return tlsCfg, nil
+}
---
+++ client/standard/service_test.go
@@ -0,0 +1,450 @@
+// Copyright © 2026 Attestant Limited.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package standard_test
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/attestantio/go-certmanager/client/standard"
+	"github.com/attestantio/go-certmanager/fetcher/majordomo"
+	certtesting "github.com/attestantio/go-certmanager/testing"
+	"github.com/attestantio/go-certmanager/testing/mock"
+	"github.com/stretchr/testify/require"
+	fileconfidant "github.com/wealdtech/go-majordomo/confidants/file"
+	majordomostandard "github.com/wealdtech/go-majordomo/standard"
+)
+
+// newMajordomo creates a majordomo service with file confidant registered.
+func newMajordomo(t *testing.T) *majordomostandard.Service {
+	t.Helper()
+	ctx := context.Background()
+
+	majordomoSvc, err := majordomostandard.New(ctx)
+	require.NoError(t, err)
+
+	// Register file confidant for file:// URIs
+	fileConf, err := fileconfidant.New(ctx)
+	require.NoError(t, err)
+	err = majordomoSvc.RegisterConfidant(ctx, fileConf)
+	require.NoError(t, err)
+
+	return majordomoSvc
+}
+
+func TestNew(t *testing.T) {
+	ctx := context.Background()
+
+	tests := []struct {
+		name    string
+		params  func(t *testing.T) []standard.Parameter
+		wantErr bool
+	}{
+		{
+			name: "Success",
+			params: func(t *testing.T) []standard.Parameter {
+				t.Helper()
+				fetcher := mock.NewFetcher(map[string][]byte{
+					"cert.pem": []byte(certtesting.ClientTest01Crt),
+					"cert.key": []byte(certtesting.ClientTest01Key),
+				})
+				return []standard.Parameter{
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+				}
+			},
+			wantErr: false,
+		},
+		{
+			name: "SuccessWithCA",
+			params: func(t *testing.T) []standard.Parameter {
+				t.Helper()
+				fetcher := mock.NewFetcher(map[string][]byte{
+					"cert.pem": []byte(certtesting.ClientTest01Crt),
+					"cert.key": []byte(certtesting.ClientTest01Key),
+					"ca.pem":   []byte(certtesting.CACrt),
+				})
+				return []standard.Parameter{
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+					standard.WithCACertURI("ca.pem"),
+				}
+			},
+			wantErr: false,
+		},
+		{
+			name: "NoFetcher",
+			params: func(t *testing.T) []standard.Parameter {
+				t.Helper()
+				return []standard.Parameter{
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+				}
+			},
+			wantErr: true,
+		},
+		{
+			name: "NoCertPEMURI",
+			params: func(t *testing.T) []standard.Parameter {
+				t.Helper()
+				fetcher := mock.NewFetcher(map[string][]byte{})
+				return []standard.Parameter{
+					standard.WithFetcher(fetcher),
+					standard.WithCertKeyURI("cert.key"),
+				}
+			},
+			wantErr: true,
+		},
+		{
+			name: "NoCertKeyURI",
+			params: func(t *testing.T) []standard.Parameter {
+				t.Helper()
+				fetcher := mock.NewFetcher(map[string][]byte{})
+				return []standard.Parameter{
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+				}
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := standard.New(ctx, tt.params(t)...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGetCertificatePair(t *testing.T) {
+	ctx := context.Background()
+
+	tests := []struct {
+		name    string
+		setup   func(t *testing.T) *standard.Service
+		wantErr bool
+	}{
+		{
+			name: "Success",
+			setup: func(t *testing.T) *standard.Service {
+				t.Helper()
+				fetcher := mock.NewFetcher(map[string][]byte{
+					"cert.pem": []byte(certtesting.ClientTest01Crt),
+					"cert.key": []byte(certtesting.ClientTest01Key),
+				})
+				svc, err := standard.New(ctx,
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+				)
+				require.NoError(t, err)
+				return svc
+			},
+			wantErr: false,
+		},
+		{
+			name: "InvalidCertificate",
+			setup: func(t *testing.T) *standard.Service {
+				t.Helper()
+				fetcher := mock.NewFetcher(map[string][]byte{
+					"cert.pem": []byte("invalid"),
+					"cert.key": []byte("invalid"),
+				})
+				svc, err := standard.New(ctx,
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+				)
+				require.NoError(t, err)
+				return svc
+			},
+			wantErr: true,
+		},
+		{
+			name: "MismatchedKeyPair",
+			setup: func(t *testing.T) *standard.Service {
+				t.Helper()
+				fetcher := mock.NewFetcher(map[string][]byte{
+					"cert.pem": []byte(certtesting.ClientTest01Crt),
+					"cert.key": []byte(certtesting.ClientTest02Key), // Mismatched key
+				})
+				svc, err := standard.New(ctx,
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+				)
+				require.NoError(t, err)
+				return svc
+			},
+			wantErr: true,
+		},
+		{
+			name: "FetcherError",
+			setup: func(t *testing.T) *standard.Service {
+				t.Helper()
+				fetcher := mock.NewFetcherWithError(os.ErrNotExist)
+				svc, err := standard.New(ctx,
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+				)
+				require.NoError(t, err)
+				return svc
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			svc := tt.setup(t)
+			cert, err := svc.GetCertificatePair(ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetCertificatePair() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr {
+				require.NotNil(t, cert)
+				require.NotEmpty(t, cert.Certificate)
+			}
+		})
+	}
+}
+
+func TestGetTLSConfig(t *testing.T) {
+	ctx := context.Background()
+
+	tests := []struct {
+		name      string
+		setup     func(t *testing.T) *standard.Service
+		wantErr   bool
+		checkFunc func(t *testing.T, cfg *tls.Config)
+	}{
+		{
+			name: "WithoutCA",
+			setup: func(t *testing.T) *standard.Service {
+				t.Helper()
+				fetcher := mock.NewFetcher(map[string][]byte{
+					"cert.pem": []byte(certtesting.ClientTest01Crt),
+					"cert.key": []byte(certtesting.ClientTest01Key),
+				})
+				svc, err := standard.New(ctx,
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+				)
+				require.NoError(t, err)
+				return svc
+			},
+			wantErr: false,
+			checkFunc: func(t *testing.T, cfg *tls.Config) {
+				t.Helper()
+				require.NotNil(t, cfg)
+				require.Len(t, cfg.Certificates, 1)
+				require.Equal(t, uint16(0x0304), cfg.MinVersion) // TLS 1.3
+				require.Nil(t, cfg.RootCAs) // No CA specified
+			},
+		},
+		{
+			name: "WithCA",
+			setup: func(t *testing.T) *standard.Service {
+				t.Helper()
+				fetcher := mock.NewFetcher(map[string][]byte{
+					"cert.pem": []byte(certtesting.ClientTest01Crt),
+					"cert.key": []byte(certtesting.ClientTest01Key),
+					"ca.pem":   []byte(certtesting.CACrt),
+				})
+				svc, err := standard.New(ctx,
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+					standard.WithCACertURI("ca.pem"),
+				)
+				require.NoError(t, err)
+				return svc
+			},
+			wantErr: false,
+			checkFunc: func(t *testing.T, cfg *tls.Config) {
+				t.Helper()
+				require.NotNil(t, cfg)
+				require.Len(t, cfg.Certificates, 1)
+				require.Equal(t, uint16(0x0304), cfg.MinVersion) // TLS 1.3
+				require.NotNil(t, cfg.RootCAs) // CA was specified
+			},
+		},
+		{
+			name: "InvalidCA",
+			setup: func(t *testing.T) *standard.Service {
+				t.Helper()
+				fetcher := mock.NewFetcher(map[string][]byte{
+					"cert.pem": []byte(certtesting.ClientTest01Crt),
+					"cert.key": []byte(certtesting.ClientTest01Key),
+					"ca.pem":   []byte("invalid CA"),
+				})
+				svc, err := standard.New(ctx,
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+					standard.WithCACertURI("ca.pem"),
+				)
+				require.NoError(t, err)
+				return svc
+			},
+			wantErr: true,
+		},
+		{
+			name: "CAFetchError",
+			setup: func(t *testing.T) *standard.Service {
+				t.Helper()
+				// Fetcher with cert/key but CA fetch will fail
+				data := map[string][]byte{
+					"cert.pem": []byte(certtesting.ClientTest01Crt),
+					"cert.key": []byte(certtesting.ClientTest01Key),
+					// "ca.pem" intentionally missing
+				}
+				fetcher := mock.NewFetcher(data)
+				svc, err := standard.New(ctx,
+					standard.WithFetcher(fetcher),
+					standard.WithCertPEMURI("cert.pem"),
+					standard.WithCertKeyURI("cert.key"),
+					standard.WithCACertURI("ca.pem"),
+				)
+				require.NoError(t, err)
+				return svc
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			svc := tt.setup(t)
+			cfg, err := svc.GetTLSConfig(ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetTLSConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr && tt.checkFunc != nil {
+				tt.checkFunc(t, cfg)
+			}
+		})
+	}
+}
+
+func TestGetTLSConfigVerifiesCertificate(t *testing.T) {
+	ctx := context.Background()
+
+	fetcher := mock.NewFetcher(map[string][]byte{
+		"cert.pem": []byte(certtesting.ClientTest01Crt),
+		"cert.key": []byte(certtesting.ClientTest01Key),
+	})
+
+	svc, err := standard.New(ctx,
+		standard.WithFetcher(fetcher),
+		standard.WithCertPEMURI("cert.pem"),
+		standard.WithCertKeyURI("cert.key"),
+	)
+	require.NoError(t, err)
+
+	cfg, err := svc.GetTLSConfig(ctx)
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+
+	// Verify the certificate can be parsed
+	require.Len(t, cfg.Certificates, 1)
+	cert, err := x509.ParseCertificate(cfg.Certificates[0].Certificate[0])
+	require.NoError(t, err)
+	require.NotNil(t, cert)
+	require.Contains(t, cert.Subject.CommonName, "client-test01")
+}
+
+func TestWithFilesystem(t *testing.T) {
+	ctx := context.Background()
+
+	// Create temp directory for certificates
+	tempDir := t.TempDir()
+	certPath := filepath.Join(tempDir, "client.crt")
+	keyPath := filepath.Join(tempDir, "client.key")
+	caPath := filepath.Join(tempDir, "ca.crt")
+
+	// Write certificates to filesystem
+	err := os.WriteFile(certPath, []byte(certtesting.ClientTest01Crt), 0o600)
+	require.NoError(t, err)
+	err = os.WriteFile(keyPath, []byte(certtesting.ClientTest01Key), 0o600)
+	require.NoError(t, err)
+	err = os.WriteFile(caPath, []byte(certtesting.CACrt), 0o600)
+	require.NoError(t, err)
+
+	// Create majordomo fetcher with file confidant
+	fetcher, err := majordomo.New(ctx,
+		majordomo.WithMajordomo(newMajordomo(t)),
+	)
+	require.NoError(t, err)
+
+	// Create service using file:// URIs
+	svc, err := standard.New(ctx,
+		standard.WithFetcher(fetcher),
+		standard.WithCertPEMURI("file://"+certPath),
+		standard.WithCertKeyURI("file://"+keyPath),
+		standard.WithCACertURI("file://"+caPath),
+	)
+	require.NoError(t, err)
+
+	// Verify certificate pair can be loaded
+	certPair, err := svc.GetCertificatePair(ctx)
+	require.NoError(t, err)
+	require.NotNil(t, certPair)
+	require.NotEmpty(t, certPair.Certificate)
+
+	// Verify TLS config can be created with CA
+	cfg, err := svc.GetTLSConfig(ctx)
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+	require.NotNil(t, cfg.RootCAs)
+	require.Len(t, cfg.Certificates, 1)
+	require.Equal(t, uint16(0x0304), cfg.MinVersion) // TLS 1.3
+}
+
+func TestMultipleGetTLSConfigCalls(t *testing.T) {
+	ctx := context.Background()
+
+	fetcher := mock.NewFetcher(map[string][]byte{
+		"cert.pem": []byte(certtesting.ClientTest01Crt),
+		"cert.key": []byte(certtesting.ClientTest01Key),
+	})
+
+	svc, err := standard.New(ctx,
+		standard.WithFetcher(fetcher),
+		standard.WithCertPEMURI("cert.pem"),
+		standard.WithCertKeyURI("cert.key"),
+	)
+	require.NoError(t, err)
+
+	// Call GetTLSConfig multiple times to ensure it works consistently
+	for i := range 5 {
+		cfg, err := svc.GetTLSConfig(ctx)
+		require.NoError(t, err, "iteration %d", i)
+		require.NotNil(t, cfg, "iteration %d", i)
+		require.Len(t, cfg.Certificates, 1, "iteration %d", i)
+	}
+}
